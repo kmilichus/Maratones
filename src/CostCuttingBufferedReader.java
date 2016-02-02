d9458994432733022f61b523e8812c855e3659e8
kmilichus <groovy.kmilo@gmail.com>
2016-02-02 13:07:40 -0500

    Create CostCuttingBufferedReader.java

---
+++ src/CostCuttingBufferedReader.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class CostCuttingBufferedReader{
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
+		int veces  = Integer.parseInt(lector.readLine());
+		
+		for (int i = 1; i <= veces; i++) {
+			int[] salarios = new int[3];
+			
+			int max = 0;
+			int min = 10001;
+			int alto = 0;
+			int bajo = 0;
+			int medio = 0;
+			
+			String[] salariosString = lector.readLine().split(" ");
+			
+			for (int j = 0; j < salarios.length; j++) {
+				salarios[j] = Integer.parseInt(salariosString[j]);
+				if (salarios[j] > max) {
+					alto = j;
+					max = salarios[j];
+				}
+				if (salarios[j] < min) {
+					bajo = j;
+					min = salarios[j];
+				}
+			}
+			for (int j = 0; j < salarios.length; j++) {
+				if (j != alto && j != bajo)
+					medio = j;
+			}
+			System.out.println("Case "+i+": "+salarios[medio]);
+		}
+	}
+
+}
